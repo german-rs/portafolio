2802b8996455fde8ea7b8d425ed2f78318e995d7
german-rs <ing.germanriveros@gmail.com>
2026-09-04 14:46:13 -0400

    feat: instalar GTM vía Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   site: "https://germanriveros.cl",
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push", "gtag"],
+      },
+    }),
+  ],
 });
