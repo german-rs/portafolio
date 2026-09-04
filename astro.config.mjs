84bff0b06d499de46bb12a6fa4e0536f493a528d
german-rs <ing.germanriveros@gmail.com>
2026-09-04 15:54:25 -0400

    feat: agregar sitemap y robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://germanriveros.cl",
@@ -9,5 +10,6 @@ export default defineConfig({
         forward: ["dataLayer.push", "gtag"],
       },
     }),
+    sitemap(),
   ],
 });
